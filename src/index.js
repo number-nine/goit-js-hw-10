3827f3253f09cb4a83da99c769b12a2c05deccba
number-nine <sergii.pokhylko@gmail.com>
2023-04-13 16:49:49 +0300

    pug added

--- src/index.js
+++ src/index.js
@@ -2,14 +2,28 @@ import './css/styles.css';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import fetchCountries from './js/fetchCountries';
+// import pug from 'pug';
+
+import countryCard from './pug-templates/country-card.pug';
+import countryList from './pug-templates/country-list.pug';
 
 import 'notiflix/dist/notiflix-3.2.6.min.css';
+
+
+console.dir(countryCard);
 
 const DEBOUNCE_DELAY = 300;
 
 const searchEl = document.querySelector('#search-box');
 
 searchEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+// const countryList = pug.renderFile('./pug-templates/country-list.pug');
+// console.log("🚀 ~ file: index.js:22 ~ countryList:", countryList)
+
+
+// document.querySelector('.country-info').innerHTML = countryCard;
+// document.querySelector('.country-list').innerHTML = countryList();
 
 function onInput(e) {
   e.preventDefault();
@@ -31,10 +45,13 @@ function onInput(e) {
       if (countries.length === 1) {
         console.log('one country');
         console.log(countries[0]);
+        document.querySelector('.country-info').innerHTML = countryCard(countries[0]);
 
         // console.log(country);
       } else {
         console.log('countries collection');
+        // document.querySelector('.country-list').innerHTML =
+        //   countryList(countries);
       }
     })
     .catch(alert => {
